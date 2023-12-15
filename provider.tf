31a88bae506165e96d7c327f6a179fd9e1cfd061
Dave Costakos <david.costakos@redhat.com>
2023-12-14 18:08:08 -0800

    breaking out terraform from ansible to make it all simpler

---
+++ provider.tf
@@ -0,0 +1,20 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      AlwaysUp  = var.alwaysup
+      Name      = var.user_name
+      CreatedBy = var.created_by
+      Contact   = var.my_email_address
+      DeleteBy  = var.deleteby
+    }
+  }
+}
