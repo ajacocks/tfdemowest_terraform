3b4c6222ef2bb352274e957ae6b69b2eafc7f57d
Alexander Jacocks <alexander@redhat.com>
2024-09-12 17:26:57 -0400

    personalize and remove SSH key

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 
 variable "vpc_name" {
   type = string
-  default = "dcostakos-vpc"
+  default = "ajacocks-vpc"
 }
 
 variable "user_name" {
   type = string
-  default = "dcostakos"
+  default = "ajacocks"
 }
 
 variable "vpc_cidr" {
@@ -26,7 +26,7 @@ variable "created_by" {
 
 variable "my_email_address" {
   type = string
-  default = "dcostako@redhat.com"
+  default = "ajacocks@redhat.com"
 }
 
 variable "deleteby" {
@@ -45,7 +45,3 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "awskey" {
-  type    = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMSpM5TCA6SYxoN6UVvsEiWelX7KcuX6iAScdMUV3F8NkHNPD4MEUVKuK9jnCuQv68d03vwvi0bRX9VVUoW/taVAf/hMotQyNuybtks6EtrY7GsKo8R33VC12mQbXiouJyMgv2kuLimcMSvc8cftKfZ1MKlj3ofUZp7FRLrsv51hMpUoVaTNDbXFwbdMp0iXxqzD8QF742hFFZ0xfYGw/sJS6nGoV6T6y8WQGZFZLU1lVVUJk4q3hjt3eZcl0Jl22r0gRwMqvqHLtgOZrVO5HQuByf15fWMnnblDHD1FMt2XADLW/iBbUE5x2rjVbsq+0O9LYF3iOv6Al999Q+nE2kZjKmcFzBQM2iryVy8ycXlAYvXmYliGvjaGNKJTbVjjskRtOUz1KBZsVtHLeeYGqbWXcJNhJvrf/ATX7MgRdyLKlHijTSZjr3Ds1QgQOSLtEfwTd3Fo9zokCC8UE4xMSZTzv4th7ZMwoM9OMLPZJKNfJIqvSkhkZCBv3PJifeLis= dcostakos@Davids-MacBook-Pro"
-}
